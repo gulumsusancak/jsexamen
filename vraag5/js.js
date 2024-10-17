a97db592b382d4261fe52456d954f0957b216513
gulumsusancak <gulumsusancak74@gmail.com>
2024-10-17 10:08:12 +0200

    laatste vraag

---
+++ vraag5/js.js
@@ -0,0 +1,15 @@
+
+let getal=parseInt(prompt("geef getallen in:"));
+let som=0;
+let i=1;
+
+while(i<=getal)//zolang dat i kleiner of gelijk aan getal is mag je het volgende doen
+{
+    som+=i;//som van getallen
+    i++;
+}
+console.log("De som van de cijfers is "+som);//output is niet juist..
+
+
+
+
